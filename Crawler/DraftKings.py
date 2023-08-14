12124466f3953aa3ddb03dfd2d1c2af81ea812d9
DuckOnQuack9 <114703236+DuckOnQuack9@users.noreply.github.com>
2023-08-14 14:11:28 -0400

    Update DraftKings.py

--- Crawler/DraftKings.py
+++ Crawler/DraftKings.py
@@ -11,7 +11,7 @@ class DraftKingsScraper:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.add_argument('--disable-dev-shm-usage'
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
         self.encoding = sys.stdout.encoding
         self.table = None
